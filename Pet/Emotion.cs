61607031db178d22c80f58347f783d56020c2f62
AlexLopez75 <alex.lopez.7e9@itb.cat>
2026-02-26 12:28:52 +0100

    add: added class Pet

---
+++ Pet/Emotion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BLOC3.PA05.Tamagochi.Pet
+{
+    public enum Emotion
+    {
+        Happy = 1,
+        Sad = 2,
+        Angry = 3,
+        Tired = 4,
+        Sick = 5
+    }
+}
---
+++ Pet/Stats.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BLOC3.PA05.Tamagochi.Pet
+{
+    public class Stats
+    {
+        public int Hunger { get; init; }
+        public int Energy { get; init; }
+        public int Health { get; init; }
+        public int Fun { get; init; }
+
+        public Stats(int hunger, int energy, int health, int fun)
+        {
+            Hunger = hunger;
+            Energy = energy;
+            Health = health;
+            Fun = fun;
+        }
+    }
+}
