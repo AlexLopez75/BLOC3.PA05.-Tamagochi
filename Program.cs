5f8fbe3620133bb4892c2d502704269b6d6a5900
AlexLopez75 <alex.lopez.7e9@itb.cat>
2026-02-26 12:48:03 +0100

    fix: resolved issue with git, resolves #4

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BLOC3.PA05.Tamagochi.Pet;
+using System;
+
+namespace BLOC3.PA05.Tamagochi
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
